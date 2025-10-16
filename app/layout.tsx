f4ecbad056a7fd6055d24d6365e12fed3bb1974b
vishavk1992 <vishavdeep551@gmail.com>
2025-10-16 00:30:00 -0400

    suspense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import '@/app/ui/global.css';
-import { inter } from './ui/fonts';
+import "@/app/ui/global.css";
+import { inter } from "./ui/fonts";
 
 export default function RootLayout({
   children,
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={'${inter.className} antialiased'}>{children}</body>
+      <body className={"${inter.className} antialiased"}>{children}</body>
     </html>
   );
 }
